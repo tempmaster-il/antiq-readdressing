e00a8538f579e4a6d8aaae402176f02c943c944e
Nachum Marcus <contact@tempmaster.co.il>
2022-12-26 18:15:04 +0200

    first commit

---
+++ readdress.ps1
@@ -0,0 +1,22 @@
+﻿$Env:BACNET_IFACE = '192.168.1.9'
+function LifeCheck {
+    param (
+        $Instance
+    )
+    .\bacrp.exe $Instance 8 1 44
+    if ($LASTEXITCODE -ne 0) {
+        # ReadIt $Instance
+        Write-Host "error"
+    }
+}
+
+
+function CreateAddressAtomicFile {
+    param (
+        $NewInstance,
+        $NewMAC
+    )
+    .\BACAF1.EXE $NewInstance $NewMAC "$NewInstance.F1"
+}
+LifeCheck -Instance 1
+CreateAddressAtomicFile -NewInstance 1 -NewMAC 1
