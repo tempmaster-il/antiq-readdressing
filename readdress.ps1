9b2cb9b9d4f384a08964d1961517f9dee99b5068
Nachum Marcus <contact@tempmaster.co.il>
2023-01-16 14:52:27 +0200

    new readdressing sheet,make floors,csv empty array

--- readdress.ps1
+++ readdress.ps1
@@ -37,25 +37,42 @@ function Readdress {
     CreateAddressAtomicFile -NewInstance $NewInstance -NewMAC $NewMAC
     #WriteAddressAtomicFile -DeviceInstance $CurrentInstance -FileName $NewInstance
     #RestartDevice -Instance $CurrentInstance
-    #Start-Sleep -Seconds 3
+    #Start-Sleep -Seconds 5
     #LifeCheck -Instance $NewInstance
 }
 Set-PSDebug -Trace 0
-$Env:BACNET_IFACE = Read-Host "Please enter the BACnet Interface IP"
+#$Env:BACNET_IFACE = Read-Host "Please enter the BACnet Interface IP"
 #$Empty = [int](Read-Host "Please enter the start of a empty range")
-$CSV = Import-Csv -Path ".\substitution.csv"
+$CSV = Import-Csv -Path ".\2Long.csv"
+# $Total = (.\bacwi.exe)[-1].Split("; Total Devices: ")[-1]
+$Empty = @()
+$LastOccIndex = 0
+for ($i = 1; $i -lt 128; $i++) {
+    $Occupied = $null
+    for ($j = $LastOccIndex; $j -lt $CSV.Count; $j++) {
+        if ($CSV[$j].CurrentMAC -eq $i) {
+            $Occupied = $CSV[$j].CurrentMAC
+            $LastOccIndex = $j++
+            break
+        }
+    }
+    if ($i -ne $Occupied) {
+        $Empty += $i
+    }
+}
 function RecursivelyReaddress {
     param (
         $FirstDevice
     )
-    # foreach ($SecondDevice in $CSV) {
-    #     if($FirstDevice.NewMAC -eq $SecondDevice.CurrentMAC) {
-    #         Readdress -CurrentInstance $SecondDevice.CurrentInstance -CurrentMAC $SecondDevice.CurrentMAC -NewInstance $SecondDevice.CurrentInstance -NewMAC $Empty
-    #         $SecondDevice.CurrentMAC = $Empty
-    #         $Empty++
-    #         RecursivelyReaddress -FirstDevice $SecondDevice
-    #     }
-    # }
+    foreach ($SecondDevice in $CSV) {
+        $EmptyIndex = 0
+        if($FirstDevice.NewMAC -eq $SecondDevice.CurrentMAC) {
+            Readdress -CurrentInstance $SecondDevice.CurrentInstance -CurrentMAC $SecondDevice.CurrentMAC -NewInstance $SecondDevice.CurrentInstance -NewMAC $Empty
+            $SecondDevice.CurrentMAC = $Empty[$EmptyIndex]
+            $EmptyIndex++
+            RecursivelyReaddress -FirstDevice $SecondDevice
+        }
+    }
     Readdress -CurrentInstance $FirstDevice.CurrentInstance -CurrentMAC $FirstDevice.CurrentMAC -NewInstance $FirstDevice.NewInstance -NewMAC $FirstDevice.NewMAC
     $FirstDevice.CurrentMAC = $FirstDevice.NewMAC
     $FirstDevice.CurrentInstance = $FirstDevice.NewInstance
